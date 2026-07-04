263858e0ded3336d4ee22e3209f6c9fd79fe1d41
ssdsd <tanushprajapati403@gmail.com>
2026-07-05 00:58:48 +0530

    complete

--- lec15-authentication(frontend)/src/protectedRoutes/MainRoutes.jsx
+++ lec15-authentication(frontend)/src/protectedRoutes/MainRoutes.jsx
@@ -3,7 +3,11 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 
 const MainRoutes = () => {
-  const { user } = useSelector((store) => store.auth);
+  const { user , isLoading} = useSelector((store) => store.auth);
+
+  if (isLoading) {
+    return <div>Loading...</div>; 
+  }
 
   if (!user) {
     return <Navigate to="/" />;
