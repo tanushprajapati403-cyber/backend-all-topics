d76bba7490e981876959630530005393ffd1959f
ssdsd <tanushprajapati403@gmail.com>
2026-07-04 15:49:32 +0530

    new added

--- lec15-authentication(backend)/server.js
+++ lec15-authentication(backend)/server.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./src/config/db");
 const userRouter = require("./src/routers/user.routes");
 const homeRouter = require("./src/routers/home.routes");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  }),
+);
 
 connectDB();
 
--- lec15-authentication(backend)/src/routers/user.routes.js
+++ lec15-authentication(backend)/src/routers/user.routes.js
@@ -3,8 +3,17 @@ const {
   registerController,
   loginController,
 } = require("../controllers/user.controller");
+const authMiddleware = require("../middlewaare/auth.middleware");
 
 const router = express.Router();
+
+router.get("/getMe", authMiddleware, (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "current loggedin user fetched",
+    data: req.user,
+  });
+});
 
 router.post("/register", registerController);
 router.post("/login", loginController);
